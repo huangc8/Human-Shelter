da5ab631202e42552f1fa747b46e4e6d63b70bfc
William Pheloung <wpheloung@gmail.com>
2015-03-02 23:23:29 -0500

    Looks good for now

---
+++ game/Assets/Scripts/Report.cs
@@ -0,0 +1,12 @@
+﻿/// <summary>
+/// Report. Contains information on success of each task
+/// </summary>
+using UnityEngine;
+using System.Collections;
+
+public class Report : MonoBehaviour{
+	string _message = "";
+	public void SetMessage(string message){
+		_message = message;
+	}
+}
