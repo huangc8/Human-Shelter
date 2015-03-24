b6c9fc65f7e7eccba4085a528bf4ecacfeb9f278
William Pheloung <wpheloung@gmail.com>
2015-03-24 18:47:18 -0400

    dictionary key changed from integer to string

--- game/Assets/Scripts/Conditions.cs
+++ game/Assets/Scripts/Conditions.cs
@@ -1,4 +1,17 @@
-﻿using UnityEngine;
+﻿/// <summary>
+/// Conditions. Acts as an interface between the dialog toolkit and the main game. Essentially this class contains a dictionary of boolean values.
+/// This dictionary will map strings to boolean values that are either true or false. The boolean values will operate as basic commands.
+/// The three different commands we are starting with include
+/// 
+/// execute <name>
+/// decline <name>
+/// invite <name>
+/// 
+/// gainApproval <name>
+/// loseApproval <name>
+/// 
+/// </summary>
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -6,7 +19,7 @@ public class Conditions : MonoBehaviour
 {
 
 		// ============================================== data
-		private Dictionary<int, bool> _conditionBase;
+		private Dictionary <string, bool> _conditionBase;
 
 		// ============================================== initialize
 
@@ -15,13 +28,13 @@ public class Conditions : MonoBehaviour
 		{
 
 				// initiate data base
-				_conditionBase = new Dictionary<int, bool> ();
+				_conditionBase = new Dictionary<string, bool> ();
 
 				// ==================== PLEASE CREATE BOOLEAN HERE!!!! =====================
 				// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 				// add conditions index	value	  condition detail
-				_conditionBase.Add (0, false); // Whether Marina is invited into camp
+				_conditionBase.Add ("invite Marina", false); // Whether Marina is invited into camp
 
 
 
@@ -34,12 +47,12 @@ public class Conditions : MonoBehaviour
 		// ============================================== functions
 
 		// get conditions 
-		public bool getCondition (int index)
+		public bool getCondition (string key)
 		{
 
 				bool tmp = false; // tmp value
 
-				if (_conditionBase.TryGetValue (index, out tmp)) {
+				if (_conditionBase.TryGetValue (key, out tmp)) {
 						return tmp; // return boolean
 				} else {
 						Debug.Log ("Error: No such condition"); // return error message
@@ -48,12 +61,12 @@ public class Conditions : MonoBehaviour
 		}
 
 		// set conditions
-		public void setCondition (int index, bool cond)
+		public void setCondition (string key, bool cond)
 		{
 				bool tmp = false; // tmp value
 		
-				if (_conditionBase.TryGetValue (index, out tmp)) {
-						_conditionBase [index] = cond; // set condition
+				if (_conditionBase.TryGetValue (key, out tmp)) {
+						_conditionBase [key] = cond; // set condition
 				} else {
 						Debug.Log ("Error: No such condition"); // return error message
 				}
