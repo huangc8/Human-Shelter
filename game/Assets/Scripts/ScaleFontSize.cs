8ffdc6ff7fdbfab7fda7d254703806a38dd759c9
Chi-Ning Huang <huangc8@rpi.edu>
2015-04-17 02:23:38 -0400

    Add in scaling Dialogue GUI

---
+++ game/Assets/Scripts/ScaleFontSize.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using PixelCrushers.DialogueSystem.UnityGUI;
+
+public class ScaleFontSize : MonoBehaviour {
+	[System.Serializable]
+	public class StyleScale {
+		public string styleName = string.Empty;
+		public float scaleFactor = 0.04f;
+	}
+	public StyleScale[] styles = new StyleScale[0];
+	private GUIRoot guiRoot = null;
+	private float lastScreenHeight = 0f;
+	void Awake() {
+		guiRoot = GetComponent<GUIRoot>();
+	}
+	void OnGUI() {
+		if (guiRoot == null || guiRoot.guiSkin == null) return;
+		if (Screen.height == lastScreenHeight) return;
+		lastScreenHeight = Screen.height;
+		foreach (var style in styles) {
+			GUIStyle guiStyle = guiRoot.guiSkin.GetStyle(style.styleName);
+			if (guiStyle != null) {
+				guiStyle.fontSize = (int) (style.scaleFactor * Screen.height);
+				// guiStyle.fixedHeight = 0;
+				// guiStyle.fixedWidth = 0;
+			}
+		}
+		guiRoot.ManualRefresh();
+	}
+}
